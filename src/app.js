63a31796cae2e96e6489fa379ab1d813ab8725e7
Jose Ricardo da Silva Dainese <josericardodainese@gmail.com>
2020-03-10 22:46:50 -0300

    Add Routes

--- src/app.js
+++ src/app.js
@@ -1,25 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3001
-const allEvents = [
-    {
-        name: 'QConSP',
-        data: ['24/04/2017', '25/04/2017', '26/04/2017'],
-        link: 'http://qconsp.com'
-    },
-    {
-        name: 'FrontInSampa',
-        data: ['01/07/2017'],
-        link: 'http://www.frontinsampa.com.br'
-    },
-    {
-        name: 'FrontInVale',
-        data: ['16/07/2017'],
-        link: 'https://www.eventick.com.br/frontinvale2017'
-    }
-]
-app.get('/allevents', (req, res) => res.json(allEvents))
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`)
-    console.log('To shutdown the server: ctrl + c')
-})
+// Bring in our dependencies
+const app = require('express')();
+const routes = require('./routes');
+
+//  Connect all our routes to our application
+app.use('/', routes);
+
+// Turn on that server!
+app.listen(3000, () => {
+  console.log('App listening on port 3000');
+});
